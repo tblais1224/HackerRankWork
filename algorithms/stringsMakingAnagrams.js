58a7c034f3097d250a4a5633d7f910e29e50c5d3
tblais1224 <thomasblais.student@careerdevs.com>
2019-11-12 20:50:11 -0500

    completed strings making anagrams

--- algorithms/stringsMakingAnagrams.js
+++ algorithms/stringsMakingAnagrams.js
@@ -1,21 +1,31 @@
 
 // Complete the makeAnagram function below.
 function makeAnagram(a, b) {
-    let newA = a.split('').filter(char => b.includes(char)).join('')
-    let newB = b.split('').filter(char => a.includes(char)).join('')
-    let removed = (a.length - newA.length) + (b.length - newB.length)
-
-    for (let i = 0; i < newA.length; i++) {
-        
+    let removed = 0
+    let aArr = a.split('')
+    let bArr = b.split('')
+    for (let i = 0; i < a.length; i++) {
+        if (bArr.includes(a[i])) {
+            bArr.splice(bArr.indexOf(a[i]),1)
+            aArr.splice(aArr.indexOf(a[i]),1)
+        }else{
+            removed++;
+        }
     }
-    console.log(newA, newB)
+    for (let i = 0; i < bArr.length; i++) {
+        if (aArr.includes(bArr[i])) {
+            aArr.splice(aArr.indexOf(bArr[i]),1)
+        }else{
+            removed++;
+        }
+    }
     return removed
 }
 
 function main() {
-    const a = "tttttttttttttttttttttttttttttttttttttsssssssssssssssss";
+    const a = "bugexikjevtubidpulaelsbcqlupwetzyzdvjphn";
 
-    const b = "sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss";
+    const b = "lajoipfecfinxjspxmevqxuqyalhrsxcvgsdxxkacspbchrbvvwnvsdtsrdk";
 
     // 30 = result
     console.log(makeAnagram(a, b)) ;
